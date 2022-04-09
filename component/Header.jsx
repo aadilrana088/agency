5c71a07ad7c7a19dcac179509035ec6ace76a115
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-04-10 05:12:39 +0530

    Mobile view and Header done

--- component/Header.jsx
+++ component/Header.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
-import React from "react";
+import { useState } from "react";
 import style from "../styles/Header.module.css";
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
         <div className={style.container}>
             <Link href="/">AV0CAD0</Link>
@@ -20,6 +21,35 @@ const Header = () => {
                     <Link href="/products/photography">PHOTOGRAPHY</Link>
                 </li>
                 <li className={style.listItem}>
+                    <Link href="/contact">CONTACT</Link>
+                </li>
+            </ul>
+            <div className={style.hamburger} onClick={() => setOpen(!open)}>
+                <div className={style.line} />
+                <div className={style.line} />
+                <div className={style.line} />
+            </div>
+            <ul
+                onClick={() => setOpen(false)}
+                className={style.menu}
+                style={{ right: open ? "0px" : "-50vw" }}
+            >
+                <li className={style.menuItem}>
+                    <Link href="/">HOME</Link>
+                </li>
+                <li className={style.menuItem}>
+                    <Link href="/products/design">DESIGN</Link>
+                </li>
+                <li className={style.menuItem}>
+                    <Link href="/products/development">DEVELOPMENT</Link>
+                </li>
+                <li className={style.menuItem}>
+                    <Link href="/products/production">PRODUCTION</Link>
+                </li>
+                <li className={style.menuItem}>
+                    <Link href="/products/photography">PHOTOGRAPHY</Link>
+                </li>
+                <li className={style.menuItem}>
                     <Link href="/contact">CONTACT</Link>
                 </li>
             </ul>
